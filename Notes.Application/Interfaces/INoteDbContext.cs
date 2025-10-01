3c84772ef91a505e05d8a773390ccb89bc2e4921
Envy <Envy@ENVY>
2025-10-01 10:43:15 +0400

    add: Interface INoteDbContext, class NotesDbContext

---
+++ Notes.Application/Interfaces/INoteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes.Domain;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INoteDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
