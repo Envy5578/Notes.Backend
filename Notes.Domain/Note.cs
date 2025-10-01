4dee311af2d1196286f3751a2b3ab53ededb3a8c
Envy <Envy@ENVY>
2025-10-01 09:59:46 +0400

    add: Note class fields

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -8,5 +8,12 @@ namespace Notes.Domain
 {
     public class Note
     {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+
     }
 }
