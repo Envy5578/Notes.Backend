7f46a30528a676a44fff749af183f2cbd30b41df
Envy <Envy@ENVY>
2025-10-02 11:28:28 +0400

    Add NoteDetails query, vm

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -0,0 +1,15 @@
+﻿using Notes.Application.Common.Mappings;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class NoteDetailsVm : IMapWith<Note>
+    {
+
+    }
+}
