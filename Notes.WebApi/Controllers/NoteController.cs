556bdedd4a4e28de87957efa188dc92234ef720e
Envy <Envy@ENVY>
2025-10-06 13:59:01 +0400

    Add Get, Create, GetAll, Update Create UpdateNoteDto

--- Notes.WebApi/Controllers/NoteController.cs
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Commands.CreateNote;
+using Notes.Application.Notes.Commands.UpdateNote;
 using Notes.Application.Notes.Queries.GetNoteDetails;
 using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.WebApi.Models;
 
 namespace Notes.WebApi.Controllers
 {
     public class NoteController : BaseController
     {
+        private readonly IMapper _mapper;
+        public NoteController(IMapper mapper) => _mapper = mapper;
+
         [HttpGet]
         public async Task<ActionResult<NoteListVm>> GetAll()
         {
@@ -16,6 +23,7 @@ namespace Notes.WebApi.Controllers
             var vm = await Mediator.Send(query);
             return Ok(vm);
         }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
         {
@@ -26,6 +34,23 @@ namespace Notes.WebApi.Controllers
             };
             var vm = await Mediator.Send(query);
             return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteDto createNoteDto)
+        {
+            var command = _mapper.Map<CreateNoteCommand>(createNoteDto);
+            command.UserId = UserId;
+            var noteId = await Mediator.Send(command);
+            return Ok(noteId);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateNoteDto updateNoteDto)
+        {
+            var command = _mapper.Map<UpdateNoteCommand>(updateNoteDto);
+            command.UserId = UserId;
+            await Mediator.Send(command);
+            return NoContent();
         }
     }
 }
